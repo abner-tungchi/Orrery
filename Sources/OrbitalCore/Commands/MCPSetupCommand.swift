7aa16ddbfe5801acaacaa414344a6422d05ae288
Grady Zhuo <gradyzhuo@gmail.com>
2026-04-09 19:47:42 +0800

    [REVERT] mcp setup success message back to stdout

--- Sources/OrbitalCore/Commands/MCPSetupCommand.swift
+++ Sources/OrbitalCore/Commands/MCPSetupCommand.swift
@@ -50,7 +50,7 @@ public struct MCPSetupCommand: ParsableCommand {
             // 2. Install slash commands
             try Self.installSlashCommands(projectDir: cwd)
 
-            FileHandle.standardError.write(Data("\(L10n.MCPSetup.success)\n".utf8))
+            print(L10n.MCPSetup.success)
         }
 
         static func installSlashCommands(projectDir: String) throws {
